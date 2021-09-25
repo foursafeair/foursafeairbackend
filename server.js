4160068765198eccb81fea62647944195ffe9d0e
foursafeair <88288414+foursafeair@users.noreply.github.com>
2021-09-25 17:51:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.use("/api", orderRoutes);
 
 const port = process.env.PORT || 4000;
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
